013e8dd95b719d60db1e9878ba9f9d92c8c279e0
ruoyu <hunger@jirengu.com>
2017-03-21 18:18:27 +0800

    webpack add function

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,19 +1,22 @@
 var webpack = require('webpack')
-var path  = require('path')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
     entry: path.join(__dirname, "js/app/index.js"),
     output: {
-        path: path.join(__dirname, "../public/js"),
-        filename: "index.js"
+        path: path.join(__dirname, "../public"),
+        filename: "js/index.js"
     },
     module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [ "style-loader", "css-loader", "less-loader" ]
-            }
-        ]
+        rules: [{
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader", "postcss-loader"]
+                }) //把 css 抽离出来生成一个文件
+        }]
     },
     resolve: {
         alias: {
@@ -26,6 +29,14 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery"
         }),
+        new ExtractTextPlugin("css/index.css"),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer(),
+                ]
+            }
+        })
         // new webpack.optimize.UglifyJsPlugin({
         //     compress: {
         //         warnings: false,
